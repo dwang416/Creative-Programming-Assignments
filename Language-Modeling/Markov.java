6e68fabaf6cac31922109ffb8cba2d504429b302
dwang416 <dwang416@gatech.edu>
2019-11-14 22:15:18 -0500

    start language modeling

---
+++ Language-Modeling/Markov.java
@@ -0,0 +1,15 @@
+class Markov {
+
+    private int counter = 0;
+    private String substring;
+
+    public Markov (String substring) {
+        this.substring = substring;
+    }
+
+    public void add () {
+        counter++;
+    }
+
+    public String toString()
+}
